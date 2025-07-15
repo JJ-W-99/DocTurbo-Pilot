e46e79a0b4bf89a28920710dd9f0291bb363df2d
Jeremy Waikwa <jeremywaikwa123@gmail.com>
2025-07-15 15:14:50 -0700

    chore(api): allow X-Requested-With and Accept headers in CORS

--- services/src/index.js
+++ services/src/index.js
@@ -12,7 +12,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
   res.header('Access-Control-Max-Age', '86400');
   res.header('Access-Control-Expose-Headers', 'Content-Length, X-Foo, X-Bar');
   
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 app.options('/api/*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
   res.header('Access-Control-Max-Age', '86400');
   res.header('Access-Control-Expose-Headers', 'Content-Length, X-Foo, X-Bar');
   res.sendStatus(204);
@@ -40,7 +40,7 @@ app.options('/api/*', (req, res) => {
 app.options('*', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
   res.header('Access-Control-Max-Age', '86400');
   res.header('Access-Control-Expose-Headers', 'Content-Length, X-Foo, X-Bar');
   res.sendStatus(204);
